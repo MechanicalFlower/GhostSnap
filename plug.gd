15d91e6e90fe7a9522f7c7693448e69aa651db2f
florianvazelle <ponythugflorian@gmail.com>
2023-07-30 09:54:37 +0200

    revert ci(export-game): workaround to import assets

--- plug.gd
+++ plug.gd
@@ -4,8 +4,6 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
 	plug("godot-extended-libraries/godot-debug-menu", {"commit": "e73e8371dc0fed4308b0d7ffb4ba3900da0a93ab"})
 	plug("KoBeWi/Godot-Universal-Fade", {"commit": "ddab6c26cf174fab107fbd6b4e66a5a350e83c22"})
-	plug("night-glider/Dialogue-Label", {"commit": "a211f35e4e4c255de1b640af14430ff675e31e5c"})
-	plug("KoBeWi/Godot-Universal-Fade", {"commit": "ddab6c26cf174fab107fbd6b4e66a5a350e83c22"})
 	plug("HungryProton/scatter", {"commit": "6d3f66ca5cc9f7acc93e29f126702e2b4fb4ef4d"})
 	plug("samclee/EZ-Sfx-and-Music", {"commit": "ac2b77c0336ff6edf6b26aa9342628d2ad63f359"})
 
