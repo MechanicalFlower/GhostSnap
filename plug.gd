fad833a6ceee06f338a8d640c2d31bef158935b9
florianvazelle <ponythugflorian@gmail.com>
2024-01-13 19:45:25 +0100

    build: convert the game to godot 4.2

--- plug.gd
+++ plug.gd
@@ -2,10 +2,14 @@ extends "res://addons/gd-plug/plug.gd"
 
 
 func _plugging():
-	plug("godot-extended-libraries/godot-debug-menu", {"commit": "2d5d67012c76b272173dcfb4947a65dc385fab94"})
+	plug(
+		"godot-extended-libraries/godot-debug-menu",
+		{"commit": "9d36ea23661d095198ff7fcfff2715172f73c983"}
+	)
 	plug("KoBeWi/Godot-Universal-Fade", {"commit": "f091514bba652880f81c5bc8809e0ee4498988ea"})
-	plug("HungryProton/scatter", {"commit": "9cbdd801f4ef6c77140e84ed9b0d96df2c6c5bb1"})
+	plug("HungryProton/scatter", {"commit": "6821b63a120f4c003da7e4d6f9ec28658e8337dc"})
 	plug("samclee/EZ-Sfx-and-Music", {"commit": "ac2b77c0336ff6edf6b26aa9342628d2ad63f359"})
+	plug("nisovin/godot-coi-serviceworker", {"commit": "de1be2989eda4c7d77a08b8c56cd94c769181c4e"})
 
 	# Patched version
-	plug("florianvazelle/EasyMenus", {"commit": "3b8602985191f6a128808068d250b5b336d05379"})
+	plug("florianvazelle/EasyMenus", {"commit": "4d960ef6ab9f51a206e2eb40c2cc0b8f8eced71a"})
